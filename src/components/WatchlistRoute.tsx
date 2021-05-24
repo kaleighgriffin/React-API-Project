ec485c6d52e6b8d99c6eb3fb5fde4d613bd8af09
adam <paddenadam@gmail.com>
2021-05-24 12:02:09 -0400

    got watchlist modal to work

--- src/components/WatchlistRoute.tsx
+++ src/components/WatchlistRoute.tsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { MovieContext } from "../context/movie-context";
+import './WatchListRoute.css'
 
 function Watchlist() {
 
@@ -11,6 +12,10 @@ function Watchlist() {
         setMovieInfo(index);
     }
 
+    function hideDetails(index: number) {
+        setMovieInfo(-1);
+    }
+
     return(
         <div className="Watchlist">
             <h2 className="WatchlistHeader">Watchlist</h2>
@@ -19,11 +24,14 @@ function Watchlist() {
                     <li key={i}>
                         {movie.title}
                         { showMovieInfo === i &&
-                        <div className="movieInfo">
-                            <p>{ movie.poster_path && <img src={"https://image.tmdb.org/t/p/w500"+movie.poster_path} alt="" /> }</p>
-                            <p>{movie.release_date}</p>
-                            <p>{movie.vote_average}</p>
-                            <p>{movie.overview}</p>
+                        <div className="infoContainer">
+                            <div className="movieInfo">
+                                <button onClick={() => hideDetails(i)}>Close</button>
+                                <p>{ movie.poster_path && <img src={"https://image.tmdb.org/t/p/w500"+movie.poster_path} alt="" /> }</p>
+                                <p>Release Date: {movie.release_date}</p>
+                                <p>Vote Average: {movie.vote_average}/10</p>
+                                <p>{movie.overview}</p>
+                            </div>
                         </div>
                         }
                         <button onClick={() => showDetails(i)}>See more details</button>
